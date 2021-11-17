0023e291e19ae94d7a856cf48fa4a24cdcc7c482
Piyush Gandhi <13226855+PiyuGandhi@users.noreply.github.com>
2021-11-18 01:00:47 +0530

    Add info button; handle image overflow

--- src/App.js
+++ src/App.js
@@ -4,51 +4,53 @@ import React from 'react';
 import ImageDisplay from './components/ImageDisplay';
 
 // First iteration
-// 1. Google like search box with search - form and state management, api management
+// 1. Google like search box with search - form and state management, api management - done
 // 2. Add autocomplete, multiple pages - form optimization, data handling
 // 3. Add images of the day
 // 4. Add central state management using Redux - Data manipulation, handling large data at scale,
-// 5. Add animations
+// 5. Add animations - done
 // 6. Add React optimizations in the form of useCallBack and useMemo
 
 function App() {
-  const [urls, setUrls] = React.useState([]);
   const [query, setQuery] = React.useState("");
-  const isLoading = true;
+  const [images, setImages] = React.useState([]);
 
-  const APIKEY = "YvLRI5ChIJyIdmMZEHzNnrwA7ybdLpjeaEoiypXI";
+  const [isLoading,setIsLoading] = React.useState(false);
+
+  // const APIKEY = "YvLRI5ChIJyIdmMZEHzNnrwA7ybdLpjeaEoiypXI";
   const IMAGE_ENDPOINT = "https://images-api.nasa.gov/";
 
 
   const handleSubmit = e => {
+    setIsLoading(true);
 
     if (query === "") {
       alert("Please enter a query.");
+          setIsLoading(false);
+
       return;
     }
 
     axios.get(IMAGE_ENDPOINT + `search?q=${query}&media_type=image`).then(resp => {
-      console.log(resp['data']['collection'])
-      const data = resp['data']['collection']['items'].map(ele => {return ele['links'][0]['href']});
-      setUrls(data)
+      const data = resp['data']['collection']['items'];
+      setImages(data)
     }).catch(err => console.error(err));
-  
-  } 
-  React.useEffect(() => { console.log("URLS: ", urls) }, [urls])
-  React.useEffect(()=>{console.log("Query ",query)},[query])
+        
+    setIsLoading(false);
 
+  } 
+  
   return (
     <div className="w-full h-screen m-auto flex flex-col">
       <div className="m-auto my-6 w-full flex flex-col">
         <p className='mx-auto text-4xl font-semibold'>Nasa Image Search</p>
         <input className="border-2 w-4/12 mx-auto border-blue-600 rounded-xl p-2 m-5 focus:ring-2 focus:ring-blue-600" placeholder="Enter Query" onChange={e => { setQuery(e.target.value) }} />
-        <button type='button' className=" flex rounded-full w-36 m-auto hover:shadow-xl hover:-translate-y-2 p-2 bg-green-500 capitalize text-white font-semibold" onClick={handleSubmit}>
-          {/* <div class="animate-spin rounded-full h-5 w-5 mx-3 border-b-2 border-gray-900" /> */}
+        <button disabled={isLoading} type='button' className={` ${isLoading ?  "opacity-10"  : "opacity-100"} flex justify-center rounded-full w-36 m-auto hover:shadow-xl hover:-translate-y-2 p-2 bg-green-500 capitalize text-white font-semibold`} onClick={handleSubmit}>
           SUBMIT
         </button>
       </div>
-    <div className="flex flex-wrap flex-row w-full">
-      {urls.length ? urls.map(url => { return <ImageDisplay url={url} />}) :""}
+    <div className="flex flex-wrap flex-row w-screen">
+      {images.length ? images.map(image => { return <ImageDisplay image={image} />}) :""}
       </div>
       </div>
   );
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,9 +1,52 @@
 import React from "react";
 
-const ImageDisplay = props => {
+
+const InfoIcon = props => {
     return (
-        <div className="transform border-2 border-gray-500 transition duration-500 hover:scale-110 m-5 h-72 w-72 max-w-72 max-h-72">
-            <img className="object-scale-down" alt={props.url} src={props.url} />
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="gray">
+  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+</svg>
+    )
+}
+
+const ToolTip = props => {
+    const [isHover, setIsHover] = React.useState(false);
+    const image = props.image;
+    const imageTitle = image['data'][0]['title'];
+    const imageDescription = image['data'][0]['description'];
+    const imageDate = image['data'][0]['date_created'];
+    
+    const handleHoverStart = e => {
+        setIsHover(true);
+    }
+
+    const handleHoverEnd = e => {
+        setIsHover(false);
+    }
+
+    return (
+        <div className="absolute z-10 right-7 top-7 flex " onMouseEnter={handleHoverStart} onMouseLeave={handleHoverEnd}>
+            <InfoIcon />
+            {isHover ? (<div className="text-xs px-5 rounded-lg overflow-scroll max-h-56 max-w-56  py-5 bg-gray-100">
+                <p className="font-semibold ">Image Title:</p> {imageTitle} <br/>
+                <p className="font-semibold ">Image Date:</p> {imageDate} <br/>
+                <p className="font-semibold ">Image Description:</p> {imageDescription} <br/>
+                </div>)
+                : ""}
+        </div>
+    )
+}
+
+const ImageDisplay = props => {
+    const image = props.image;
+    const imageUrl = image['links'][0]['href'];
+    return (
+        <div className="transform transition duration-500 hover:scale-110">
+        <ToolTip image={image}/>
+        <div className=" overflow-hidden  ">
+            
+            <img className=" border-2 object-fill border-gray-500  m-5 max-w-72 max-h-72" alt={props.url} src={imageUrl} />
+            </div>
         </div>
     )
 }
